e02133f2ce2f9f6522d7aedea3e9c568104d7042
ivanm <ivan.matuck@hotmail.com>
2025-01-25 14:55:24 -0300

    Projeto inicializado

---
+++ src/main/java/com/empresa/transferencias/agendamento_transferencias/AgendamentoTransferenciasApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.transferencias.agendamento_transferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoTransferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoTransferenciasApplication.class, args);
+	}
+
+}
