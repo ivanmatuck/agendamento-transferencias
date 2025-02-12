71adfc6fbe2951ced81074d1fd082eb2fe36253b
ivanm <ivan.matuck@hotmail.com>
2025-02-12 15:17:26 -0300

    Adicionando AWS Secrets Manager JDBC Driver

--- src/main/java/com/empresa/transferencias/AgendamentoTransferenciasApplication.java
+++ src/main/java/com/empresa/transferencias/AgendamentoTransferenciasApplication.java
@@ -2,12 +2,10 @@ package com.empresa.transferencias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import com.empresa.transferencias.config.AwsSecretsConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AwsSecretsConfig.class)
 public class AgendamentoTransferenciasApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AgendamentoTransferenciasApplication.class, args);
 	}
